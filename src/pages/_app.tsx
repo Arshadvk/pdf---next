f51b28e6582d3ce40ff4f0810d70cb0b2ea097e4
Arshadvk <arshadvk7560@gmail.com>
2024-05-30 18:58:58 +0400

    keywords

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -64,9 +64,9 @@ const App = (props: ExtendedAppProps) => {
         <title>{`${themeConfig.templateName} - People’s Cultural Forum (P.C.F)`}</title>
         <meta
           name='description'
-          content={`${themeConfig.templateName} – Material Design React Admin Dashboard Template – is the most developer friendly & highly customizable Admin Dashboard Template based on MUI v5.`}
+          content={`${themeConfig.templateName} – People’s Cultural Forum`}
         />
-        <meta name='keywords' content='Material Design, MUI, Admin Template, React Admin Template' />
+        <meta name='keywords' content='People’s Cultural Forum' />
         <meta name='viewport' content='initial-scale=1, width=device-width' />
       </Head>
 
