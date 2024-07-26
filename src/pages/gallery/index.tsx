e4ed753af8f2f8b33703dcd039c88d80baa98b7f
Arshadvk <arshadvk7560@gmail.com>
2024-07-26 21:25:25 +0400

    committ

--- src/pages/gallery/index.tsx
+++ src/pages/gallery/index.tsx
@@ -5,12 +5,22 @@ import UserLayout from 'src/@core/layouts/UserLayout'
 
 const HomePage = () => {
 
+  const photo = [1,2,3, 4,5,6,7,8,9]
+
   return (
-    <div>
+    <div className=' grid grid-cols-3'>
+       {photo.map((photo)=>{
+        return (
+            // eslint-disable-next-line react/jsx-key
+            <div className='flex items-center justify-center pb-5'>
+            <img src={`/images/events/${photo}.jpg`} height={300} width={300} alt="" srcSet="" />
+          </div>)
+       })}
+    
 
     </div>
   )
-} 
+}
 
 HomePage.getLayout = (page: ReactNode) => <UserLayout>{page}</UserLayout>
 
